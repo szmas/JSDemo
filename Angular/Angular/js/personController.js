137933ca0107aa5d9fe37708129838ce60c183af
mas <305596979@qq.com>
2017-10-18 18:43:26 +0800

    Signed-off-by: mas <305596979@qq.com>

---
+++ Angular/Angular/js/personController.js
@@ -0,0 +1,7 @@
+﻿app.controller('personCtrl2', function ($scope) {
+    $scope.firstName = "John",
+    $scope.lastName = "Doe",
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+});
